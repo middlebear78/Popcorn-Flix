631a00645577788b41fab7b970018598b90aa20c
middlebear78 <urisham@gmail.com>
2024-11-27 00:18:16 +0200

    started with the header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-    return <div className="App">app</div>;
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    );
 }
 
 export default App;
